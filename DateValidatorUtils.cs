f648e0b0571683ad41f22128d59f2c8535dff223
tvidal02 <thiago_acvidal@hotmail.com>
2016-06-28 13:05:08 -0300

    Upload do Projeto

---
+++ DateValidatorUtils.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace UtilityClasses
+{
+    /* Guideline: Como Criar uma Classe Utilitária */
+    /* 0. Veja se você pode criar métodos de extensão como alternativa
+     * 1. Crie uma classe estática stateless
+     * 2. Stateless: não insira atributos ou propriedades 
+     * 3. Crie métodos estáticos (vão ser chamados pelo nome da classe, sem instanciá-la, por isso que são sem estado)
+     * 4. Crie uma classe para testar os métodos da classe utilitária
+     * 5. Chame os métodos pelo nome da classe
+     */
+
+    public static class DateValidatorUtils
+    {
+        /// <summary>
+        /// Valida uma data string no formato dd/mm/aaaa HH:MM:SS:MMM
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static bool ValidateDate(string date)
+        {
+            return IsDateTime(date) ? true : false;
+        }
+
+        /// <summary>
+        /// Valida uma data string em horas, minutos e segundos e devolve um objeto DateTime
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime ValidateDateTime(string date) 
+        {
+            string[] arrDate = date.Split(':');
+            
+            try
+            {
+                var hour = Convert.ToInt32(arrDate[0]);
+                var min = Convert.ToInt32(arrDate[1]);
+                var sec = Convert.ToInt32(arrDate[2]);
+
+                DateTime objDate = new DateTime(hour, min, sec);
+
+                if (!IsDateTime(date))
+                    throw new Exception("Erro na validação");
+                
+                return objDate;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        
+        /// <summary>
+        /// Valida uma data string em horas, minutos, segundos e milissegundos e devolve um objeto DateTime
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime ValidateDateTimeMillis(string date)
+        {
+            string[] arrDate = date.Split(':', '.');
+
+            try
+            {
+                var hour = Convert.ToInt32(arrDate[0]);
+                var min = Convert.ToInt32(arrDate[1]);
+                var sec = Convert.ToInt32(arrDate[2]);
+                var millis = Convert.ToInt32(arrDate[3]);
+
+                DateTime objDate = new DateTime(
+                    DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day,
+                    hour, min, sec, millis);
+
+                if (!IsDateTime(date))
+                    throw new Exception("Erro na validação");
+
+                return objDate;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static bool IsDateTime(string date) 
+        {
+            DateTime value;
+
+            return DateTime.TryParse(date, out value) ? true : false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UtilityClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testes - método ValidateDate
+            DateValidatorUtils.ValidateDate("18:09:01"); //ok
+            DateValidatorUtils.ValidateDate("25/01/2006 18:09:01"); //ok
+            DateValidatorUtils.ValidateDate("25/01/2006 18:09:01.345"); //ok
+            DateValidatorUtils.ValidateDate("1/31/2016"); //erro: formato en-US
+
+            //Testes - método ValidateDateTime
+            //1- Testar string com horas, minutos e segundos
+            DateValidatorUtils.ValidateDateTime("18:09:01"); //ok
+            DateValidatorUtils.ValidateDateTime("18:09:91"); //erro
+            DateValidatorUtils.ValidateDateTime("31/05/1999 18:09:01"); //erro
+
+            //Testes - método ValidateDateTimeMillis
+            //2- Testar string com horas, minutos, segundos e milissegundos
+            DateValidatorUtils.ValidateDateTimeMillis("8:32:45.126"); //ok
+            DateValidatorUtils.ValidateDateTimeMillis("07/12/2008 8:32:45.126"); //erro
+
+            Console.ReadLine();
+        }
+    }
+}
